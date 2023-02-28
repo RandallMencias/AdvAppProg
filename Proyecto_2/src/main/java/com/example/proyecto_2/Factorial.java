7ef0856bb3ad16052d55f056cd58ff3378003aa2
JVenegasB <112045049+JVenegasB@users.noreply.github.com>
2023-02-28 03:10:11 -0500

    proyecto finalizado

--- Proyecto_2/src/main/java/com/example/proyecto_2/Factorial.java
+++ Proyecto_2/src/main/java/com/example/proyecto_2/Factorial.java
@@ -1,33 +1,59 @@
 package com.example.proyecto_2;
 
 import javafx.concurrent.Task;
+import javafx.scene.control.Label;
 
 import java.math.BigInteger;
+import java.util.concurrent.Future;
 
+//class to calculate factorial and its runtime
 public class Factorial extends Task<BigInteger> {
-    private final int n; // Factorial number to calculate
 
-    // constructor
-    public Factorial(int n) {
-        this.n = n;
+    private BigInteger f = new BigInteger("1");
+    private int start;
+    private int end;
+    private static BigInteger result = new BigInteger("1");
+    private String time;
+    //calculate runtime
+    private long startTime;
+    private long endTimeT;
+    
+    //constructor
+    public Factorial(int start, int end) {
+        this.start = start;
+        this.end = end;
+
     }
-
-    // long-running code to be run in a worker thread
+    //function to get factorial
     @Override
     protected BigInteger call() {
-        updateMessage("Calculating...");
-        BigInteger result = factorial(n);
-        updateMessage("Done calculating.");
-        return result;
-    }
-
-    public static BigInteger factorial(int N)
-    {
+        startTime = System.currentTimeMillis();
         BigInteger f = new BigInteger("1");
-
-        for (int i = 2; i <= N; i++)
+        //for loop to get the  factorial while updating the values at the GUI
+        for (int i = start; i <= end; i++) {
             f = f.multiply(BigInteger.valueOf(i));
-        return f;
-    }
-}
+            updateMessage(f.toString() + "*" + Integer.toString(i));
+            updateValue(f);
 
+            //add sleep to see the progress
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            updateProgress(i,end); 
+        }
+        //update gui labels
+        endTimeT = System.currentTimeMillis();
+        time = f.toString()+"\t tiempo: " + Long.toString(endTimeT-startTime) + " ms";
+        updateMessage(time); 
+        return f;
+
+    }
+    //function to get runTime
+    public String getRunTime(){
+        return time;
+    }
+
+
+}
--- Proyecto_2/src/main/java/com/example/proyecto_2/MultiThreadGui.java
+++ Proyecto_2/src/main/java/com/example/proyecto_2/MultiThreadGui.java
@@ -1,3 +1,8 @@
+//Juan Diego Venegas Barreto 00209856
+//Randall Mencias 00321469
+// clase: Programacion Avanzada de Apps
+//Proyecto 2. programa que usa tasks para calcular factorial
+
 package com.example.proyecto_2;
 
 import javafx.application.Application;
@@ -8,20 +13,26 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.math.BigInteger;
-import javafx.scene.control.ProgressBar;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+import javafx.application.Platform;
+
+
 
 public class MultiThreadGui extends Application {
-    private static final String[] COLUMN_NAMES = {"Thread #", "Execution Time", "Memory Usage"};
+    //initialize variables
+    List<Future<BigInteger>> futures = new ArrayList<>();
     private int factorialof = 0;
     private int numThreads = 0;
-    private MultiFactorial task;
-    Label resultado;
-    ProgressBar progressBar;
+    private Pairs[] pairs;
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         // Create a new VBox to hold the initial scene
@@ -29,85 +40,67 @@ public class MultiThreadGui extends Application {
         initialVBox.setSpacing(10);
         initialVBox.setPadding(new Insets(10));
         initialVBox.setAlignment(Pos.CENTER);
-
+        GridPane gridPane = new GridPane();
         // Add a label to prompt the user to enter the number of threads to test
         Label numThreadsLabel = new Label("Enter the number of threads to test:");
         initialVBox.getChildren().add(numThreadsLabel);
         TextField numThreadsTextField = new TextField();
         initialVBox.getChildren().add(numThreadsTextField);
-
+        // Add a label to prompt the user to enter the number to calculate the factorial
         Label factorialofLabel = new Label("Enter the factorial:");
         initialVBox.getChildren().add(factorialofLabel);
         TextField factorialTextField = new TextField();
         initialVBox.getChildren().add(factorialTextField);
-         
-         
-        // Add a button to submit the user's choice and switch to the results scene
+
+        // Add a button to submit the user's input
         Button submitButton = new Button("Submit");
         submitButton.setOnAction(event -> {
             try {
                 numThreads = Integer.parseInt(numThreadsTextField.getText());
                 factorialof = Integer.parseInt(factorialTextField.getText());
-                
-                // Create a new GridPane to display the results
-                GridPane gridPane = new GridPane();
-                gridPane.setPadding(new Insets(10));
-                gridPane.setHgap(10);
-                gridPane.setVgap(10);
-                initialVBox.getChildren().add(gridPane);
-              
-              // Add column labels to the top row of the table
-                for (int i = 0; i < COLUMN_NAMES.length; i++) {
-                    Label label = new Label(COLUMN_NAMES[i]);
-                    gridPane.add(label, i, 0);
-                }
-                gridPane.add(new Button(),2,2);
-                      
-               
-                
-                // Add a row for each thread
-                for (int i = 0; i < numThreads; i++) {
-      
-                    // Add a label to display the thread number
-                    Label threadLabel = new Label("Thread #" + (i + 1));
-                    
-                    task = new MultiFactorial(numThreads,factorialof);
-                    //Label para mostrar resultado
-                    resultado.textProperty().bind(task.messageProperty());
-                    gridPane.add(resultado,1, numThreads +1 );
-                    
-                    
-                    gridPane.add(threadLabel, 0, i + 1);
-                  
-                    //colocar progressBar
-                    //progressBar.progressProperty().bind(task.progressProperty());
-                    //gridPane.add(progressBar,1,i+1);
-      
-                  // Add labels to display the execution time and memory usage
-                    Label executionTimeLabel = new Label();
-                    Label memoryUsageLabel = new Label();
-                    gridPane.add(executionTimeLabel, 1, i + 1);
-                    gridPane.add(memoryUsageLabel, 2, i + 1);
-                  
-                   
-                  
-              }
-              
+                Divide divide = new Divide(factorialof, numThreads);
+                pairs = divide.getPairs();
             } catch (NumberFormatException e) {
                 // If the user entered an invalid number, display an error message
                 Label errorLabel = new Label("Please enter a valid integer");
                 initialVBox.getChildren().add(errorLabel);
             }
         });
-        
+        //add button to calculate with given variables
+        Button runButton = new Button("Run");
         initialVBox.getChildren().add(submitButton);
-        
-        
+        initialVBox.getChildren().add(runButton);
+        runButton.setOnAction(event -> {
+            ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+            //loop to create multiple tasks and their respective labels
+            for (int i = 1; i < numThreads + 1; i++) {
+                Factorial task = new Factorial(pairs[i - 1].getStart(), pairs[i - 1].getEnd());
+                Future<BigInteger> future = (Future<BigInteger>) executor.submit(task::call);
+                futures.add(future);
+                Label label = new Label();
+                label.textProperty().bind(task.messageProperty());
+                gridPane.add(label, 0, i);
+            }
+            //get and show results with their respective labels
+            executor.submit(() -> {
+                Platform.runLater(() -> {
+                    Factorial noThread = new Factorial(1, factorialof);
+                    Label lb = new Label();
+                    noThread.call().toString();
+                    gridPane.add(new Label("Resultado sin threads:  " +  noThread.getRunTime()), 0, numThreads+2);
+                    gridPane.add(resultLabel(), 0, numThreads + 1);
+                });
+            });
 
+        });
+        gridPane.setPadding(new Insets(10));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.add(new Label("Thread #"), 0, 0);
 
-        
-        
-        
+        // Add column labels to the top row of the table
+
+        initialVBox.getChildren().add(gridPane);
         // Create a new Scene with the initial VBox as its root node
         Scene initialScene = new Scene(initialVBox, 400, 300);
 
@@ -115,60 +108,24 @@ public class MultiThreadGui extends Application {
         primaryStage.setTitle("Parallel Programming Task");
         primaryStage.setScene(initialScene);
         primaryStage.show();
+
     }
-
-    /*private Scene createResultsScene() {
-
-        // Create a new GridPane to display the results
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10));
-        gridPane.setHgap(10);
-        gridPane.setVgap(10);
-
-        // Add column labels to the top row of the table
-        for (int i = 0; i < COLUMN_NAMES.length; i++) {
-            Label label = new Label(COLUMN_NAMES[i]);
-            gridPane.add(label, i, 0);
+    //function to calculate and show result in label
+    private Label resultLabel() {
+        BigInteger results = BigInteger.ONE;
+        for (Future<BigInteger> future1 : futures) {
+            try {
+                results = results.multiply((BigInteger)future1.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
         }
-        
-        
-        // Add a row for each thread
-        for (int i = 0; i < numThreads; i++) {
-
-            // Add a label to display the thread number
-            Label threadLabel = new Label("Thread #" + (i + 1));
-            ProgressBar progressBar = new ProgressBar();
-            progressBar.progressProperty().bind(task.progressProperty());
-            gridPane.add(threadLabel, 0, i + 1);
-            
-            //colocar progressBar
-            gridPane.add(progressBar,1,i+1);
-
-            // Add labels to display the execution time and memory usage
-            Label executionTimeLabel = new Label();
-            Label memoryUsageLabel = new Label();
-            gridPane.add(executionTimeLabel, 1, i + 1);
-            gridPane.add(memoryUsageLabel, 2, i + 1);
-            
-            
-        }
-        //Label para mostrar resultado
-        
-        gridPane.add(resultado,1,numThreads+1);
-        Scene resultsScene = new Scene(gridPane, 400, 300);
-
-        return resultsScene;
-    }*/
-
-
-
-
+        Label label = new Label("Resultado con Threads: "+results.toString());
+        return label;
+    }
 
     public static void main(String[] args) {
-//        launch();
-        /*MultiFactorial multi = new MultiFactorial(11, 3);
-        BigInteger a = multi.factorial();
-        System.out.println(a);*/
-        launch(args);
+        launch();
     }
 }
+
