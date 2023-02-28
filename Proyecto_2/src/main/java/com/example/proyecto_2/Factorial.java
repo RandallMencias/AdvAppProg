af9cda02bd9741f9b22458f445842511ae267560
RandallMencias <randallmencias@gmail.com>
2023-02-28 01:28:15 -0500

    Funciona Resultados y sin threads

--- Proyecto_2/src/main/java/com/example/proyecto_2/Factorial.java
+++ Proyecto_2/src/main/java/com/example/proyecto_2/Factorial.java
@@ -1,33 +1,58 @@
 package com.example.proyecto_2;
 
+
 import javafx.concurrent.Task;
 
 import java.math.BigInteger;
+import java.util.concurrent.Future;
 
+//class to calculate factorial and its runtime
 public class Factorial extends Task<BigInteger> {
-    private final int n; // Factorial number to calculate
 
-    // constructor
-    public Factorial(int n) {
-        this.n = n;
+    private BigInteger f = new BigInteger("1");
+    private int start;
+    private int end;
+    //calculate runtime
+    private long startTime = System.currentTimeMillis();
+    private long endTimeT;
+    private static BigInteger result = new BigInteger("1");
+    //constructor
+    public Factorial(int start, int end) {
+        this.start = start;
+        this.end = end;
+
     }
-
-    // long-running code to be run in a worker thread
+    //function to get factorial
     @Override
     protected BigInteger call() {
-        updateMessage("Calculating...");
-        BigInteger result = factorial(n);
-        updateMessage("Done calculating.");
-        return result;
+        BigInteger f = new BigInteger("1");
+        for (int i = start; i <= end; i++) {
+
+            f = f.multiply(BigInteger.valueOf(i));
+            updateMessage(f.toString() + "*" + Integer.toString(i));
+            updateValue(f);
+
+            //add sleep to see the progress
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//
+        }
+        updateMessage(f.toString());
+        endTimeT = System.currentTimeMillis();
+
+        return f;
+
+    }
+    //function to get runTime
+    public long getRunTime(){
+        return (endTimeT - startTime);
     }
 
-    public static BigInteger factorial(int N)
-    {
-        BigInteger f = new BigInteger("1");
-
-        for (int i = 2; i <= N; i++)
-            f = f.multiply(BigInteger.valueOf(i));
-        return f;
+    public BigInteger getResult(){
+        return result;
     }
 }
 
