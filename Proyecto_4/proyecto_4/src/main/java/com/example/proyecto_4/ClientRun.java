1b0c6115b844d3cb87a2f9055890fba932bf875c
RandallMencias <randallmencias@gmail.com>
2023-03-25 18:42:13 -0500

    Sin conectar a gui

--- Proyecto_4/proyecto_4/src/main/java/com/example/proyecto_4/ClientRun.java
+++ Proyecto_4/proyecto_4/src/main/java/com/example/proyecto_4/ClientRun.java
@@ -15,8 +15,7 @@ public class ClientRun extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
