cb4ebf86789b5f7dd50cc7b9e4a532bb97c1b279
RandallMencias <randallmencias@gmail.com>
2023-04-28 17:00:43 -0500

    Image Load Test Correct

--- Proyecto_6/app/src/main/java/com/example/proyecto_6/AsyncTasks.java
+++ Proyecto_6/app/src/main/java/com/example/proyecto_6/AsyncTasks.java
@@ -3,11 +3,16 @@ package com.example.proyecto_6;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 class LoadImageTask extends AsyncTask<String,Void, Bitmap> {
     private ImageView imageView;
@@ -53,3 +58,32 @@ class LoadImageTask extends AsyncTask<String,Void, Bitmap> {
     }
 }
 
+class html extends AsyncTask<String,Void,Void>{
+    private URL url;
+    private ArrayList<String> lista;
+    public html(URL url){
+        lista = new ArrayList<>();
+        this.url = url;
+    }
+    @Override
+    protected Void doInBackground(String... strings) {
+        try (
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));){
+            while (reader.readLine() != null){
+                lista.add(reader.readLine());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+    @Override
+    protected void onPostExecute(Void aVoid){
+        for (int i = 75; i < 80; i++) {
+            Log.d("html", "onPostExecute: "+lista.get(i));
+        }
+    }
+
+
+}
---
+++ Proyecto_6/app/src/main/java/com/example/proyecto_6/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.proyecto_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ImageView iv = (ImageView) findViewById(R.id.imageView1);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity", "onClick: ");
+                //new LoadImageTask(iv).execute("https://d2pn8kiwq2w21t.cloudfront.net/original_images/jpegPIA04628.jpg");
+                try {
+                    new html(new URL("https://nasasearch.nasa.gov/search?affiliate=nasa&page=6&query=%2A.jpg&sort_by=&utf8=%E2%9C%93")).execute();
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
+        });
+
+
+
+    }
+
+
+
+}
