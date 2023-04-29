c91dc197aa968128225506159dead98dc2e6a6ee
JVenegasB <112045049+JVenegasB@users.noreply.github.com>
2023-04-28 20:00:13 -0500

    Singleton con cache

--- Proyecto_6/app/src/main/java/com/example/proyecto_6/singleton.java
+++ Proyecto_6/app/src/main/java/com/example/proyecto_6/singleton.java
@@ -1,14 +1,20 @@
 package com.example.proyecto_6;
 
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class singleton {
     private static Map<String , Nasa> mapaImagenes;
+    private static Map<String, Bitmap> cache;
     private static singleton instancia = new singleton();
     public static singleton getInstance(){return instancia; }
     private singleton(){
         mapaImagenes = new HashMap<>();
+        cache = new HashMap<>();
     }
 
     public void addNasa(Nasa nasa){
@@ -16,5 +22,22 @@ public class singleton {
             mapaImagenes.put(nasa.getTitle(),nasa);
         }
     }
+    public void addtoCache(String name, Bitmap bitmap){
+        if(!cache.containsKey(name)) {
+            cache.put(name,bitmap);
+        }
+    }
 
+    public Bitmap getBitmap(String name){
+        return cache.get(name);
+    }
+    public List<String> getBitmapName(){
+        List<String> listaNombres = new ArrayList<>();
+        for(String nombre : cache.keySet()){
+            listaNombres.add(nombre);
+        }
+        return listaNombres;
+    }
+
+    //private singleton s = singleton.getInstance(); //instancia de la clase singleton
 }
