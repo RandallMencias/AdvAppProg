91b326730343347e460d5c619833cbb1d8e04d4c
JVenegasB <112045049+JVenegasB@users.noreply.github.com>
2023-04-28 19:41:24 -0500

    Singleton

--- Proyecto_6/app/src/main/java/com/example/proyecto_6/singleton.java
+++ Proyecto_6/app/src/main/java/com/example/proyecto_6/singleton.java
@@ -1,4 +1,20 @@
 package com.example.proyecto_6;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class singleton {
+    private static Map<String , Nasa> mapaImagenes;
+    private static singleton instancia = new singleton();
+    public static singleton getInstance(){return instancia; }
+    private singleton(){
+        mapaImagenes = new HashMap<>();
+    }
+
+    public void addNasa(Nasa nasa){
+        if(!mapaImagenes.containsKey(nasa.getTitle())){
+            mapaImagenes.put(nasa.getTitle(),nasa);
+        }
+    }
+
 }
